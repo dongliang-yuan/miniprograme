459fa54b2932f6d3baba0ee5fabb94cecb9bb03b
dongliang.yuan <dongliang.yuan@decagames.cn>
2025-12-08 19:01:16 +0800

    update the database

--- miniprogram-3/pages/usercenter/index.js
+++ miniprogram-3/pages/usercenter/index.js
@@ -1,233 +1,116 @@
-import { fetchUserCenter } from '../../services/usercenter/fetchUsercenter';
 import Toast from 'tdesign-miniprogram/toast/index';
 
-const menuData = [
-  [
-    {
-      title: '收货地址',
-      tit: '',
-      url: '',
-      type: 'address',
-    },
-    {
-      title: '优惠券',
-      tit: '',
-      url: '',
-      type: 'coupon',
-    },
-    {
-      title: '积分',
-      tit: '',
-      url: '',
-      type: 'point',
-    },
-  ],
-  [
-    {
-      title: '帮助中心',
-      tit: '',
-      url: '',
-      type: 'help-center',
-    },
-    {
-      title: '客服热线',
-      tit: '',
-      url: '',
-      type: 'service',
-      icon: 'service',
-    },
-  ],
-];
-
-const orderTagInfos = [
-  {
-    title: '待付款',
-    iconName: 'wallet',
-    orderNum: 0,
-    tabType: 5,
-    status: 1,
-  },
-  {
-    title: '待发货',
-    iconName: 'deliver',
-    orderNum: 0,
-    tabType: 10,
-    status: 1,
-  },
-  {
-    title: '待收货',
-    iconName: 'package',
-    orderNum: 0,
-    tabType: 40,
-    status: 1,
-  },
-  {
-    title: '待评价',
-    iconName: 'comment',
-    orderNum: 0,
-    tabType: 60,
-    status: 1,
-  },
-  {
-    title: '退款/售后',
-    iconName: 'exchang',
-    orderNum: 0,
-    tabType: 0,
-    status: 1,
-  },
-];
-
-const getDefaultData = () => ({
-  showMakePhone: false,
-  userInfo: {
-    avatarUrl: '',
-    nickName: '正在登录...',
-    phoneNumber: '',
-  },
-  menuData,
-  orderTagInfos,
-  customerServiceInfo: {},
-  currAuthStep: 1,
-  showKefu: true,
-  versionNo: '',
-});
+const emptyUser = { avatarUrl: '', nickName: '', phoneNumber: '' };
 
 Page({
-  data: getDefaultData(),
-
-  onLoad() {
-    this.getVersionInfo();
+  data: {
+    authed: false,
+    userInfo: emptyUser,
+    countsData: {
+      coupon: 0,
+      point: 0,
+    },
   },
 
   onShow() {
-    this.getTabBar().init();
-    this.init();
-  },
-  onPullDownRefresh() {
-    this.init();
-  },
-
-  init() {
-    this.fetUseriInfoHandle();
+    // 进入“我的”页，重置并清理本地缓存，避免旧数据残留
+    wx.removeStorageSync('userInfo');
+    wx.removeStorageSync('authToken');
+    this.resetState();
+    if (this.getTabBar && this.getTabBar()) {
+      this.getTabBar().init();
+    }
   },
 
-  fetUseriInfoHandle() {
-    fetchUserCenter().then(({ userInfo, countsData, orderTagInfos: orderInfo, customerServiceInfo }) => {
-      // eslint-disable-next-line no-unused-expressions
-      menuData?.[0].forEach((v) => {
-        countsData.forEach((counts) => {
-          if (counts.type === v.type) {
-            // eslint-disable-next-line no-param-reassign
-            v.tit = counts.num;
-          }
-        });
-      });
-      const info = orderTagInfos.map((v, index) => ({
-        ...v,
-        ...orderInfo[index],
-      }));
-      this.setData({
-        userInfo,
-        menuData,
-        orderTagInfos: info,
-        customerServiceInfo,
-        currAuthStep: 2,
-      });
-      wx.stopPullDownRefresh();
+  resetState() {
+    // 默认不读取缓存，进入页面即为未登录状态
+    this.setData({
+      authed: false,
+      userInfo: emptyUser,
+      countsData: {
+        coupon: 0,
+        point: 0,
+      },
     });
   },
 
-  onClickCell({ currentTarget }) {
-    const { type } = currentTarget.dataset;
-
-    switch (type) {
-      case 'address': {
-        wx.navigateTo({ url: '/pages/user/address/list/index' });
-        break;
-      }
-      case 'service': {
-        this.openMakePhone();
-        break;
-      }
-      case 'help-center': {
+  onAuthorize() {
+    wx.getUserProfile({
+      desc: '用于完善个人资料',
+      success: (res) => {
+        wx.login({
+          success: (loginRes) => {
+            const code = loginRes.code;
+            if (code) {
+              wx.setStorageSync('authToken', code);
+            }
+          },
+        });
+        const u = res.userInfo || {};
+        const info = {
+          avatarUrl: u.avatarUrl || '',
+          nickName: u.nickName || '用户',
+          phoneNumber: u.phoneNumber || '',
+        };
+        wx.setStorageSync('userInfo', info);
+        this.setData({
+          authed: true,
+          userInfo: info,
+        });
         Toast({
           context: this,
           selector: '#t-toast',
-          message: '你点击了帮助中心',
-          icon: '',
-          duration: 1000,
+          message: '登录成功',
         });
-        break;
-      }
-      case 'point': {
+      },
+      fail: () => {
         Toast({
           context: this,
           selector: '#t-toast',
-          message: '你点击了积分菜单',
-          icon: '',
-          duration: 1000,
+          message: '授权失败',
         });
-        break;
-      }
-      case 'coupon': {
-        wx.navigateTo({ url: '/pages/coupon/coupon-list/index' });
-        break;
-      }
-      default: {
-        Toast({
-          context: this,
-          selector: '#t-toast',
-          message: '未知跳转',
-          icon: '',
-          duration: 1000,
-        });
-        break;
-      }
-    }
+      },
+    });
   },
 
-  jumpNav(e) {
-    const status = e.detail.tabType;
-
-    if (status === 0) {
-      wx.navigateTo({ url: '/pages/order/after-service-list/index' });
-    } else {
-      wx.navigateTo({ url: `/pages/order/order-list/index?status=${status}` });
-    }
+  onLogout() {
+    wx.removeStorageSync('userInfo');
+    wx.removeStorageSync('authToken');
+    this.setData({
+      authed: false,
+      userInfo: emptyUser,
+    });
+    Toast({
+      context: this,
+      selector: '#t-toast',
+      message: '已退出登录',
+    });
   },
 
-  jumpAllOrder() {
+  gotoAddress() {
+    wx.navigateTo({ url: '/pages/user/address/list/index' });
+  },
+
+  gotoOrderList() {
     wx.navigateTo({ url: '/pages/order/order-list/index' });
   },
 
-  openMakePhone() {
-    this.setData({ showMakePhone: true });
+  gotoCoupon() {
+    wx.navigateTo({ url: '/pages/coupon/coupon-list/index' });
   },
 
-  closeMakePhone() {
-    this.setData({ showMakePhone: false });
-  },
-
-  call() {
+  callService() {
     wx.makePhoneCall({
-      phoneNumber: this.data.customerServiceInfo.servicePhone,
+      phoneNumber: '4006336868',
+      fail: () => {
+        Toast({
+          context: this,
+          selector: '#t-toast',
+          message: '拨打失败',
+        });
+      },
     });
   },
 
-  gotoUserEditPage() {
-    const { currAuthStep } = this.data;
-    if (currAuthStep === 2) {
-      wx.navigateTo({ url: '/pages/user/person-info/index' });
-    } else {
-      this.fetUseriInfoHandle();
-    }
-  },
-
-  getVersionInfo() {
-    const versionInfo = wx.getAccountInfoSync();
-    const { version, envVersion = __wxConfig } = versionInfo.miniProgram;
-    this.setData({
-      versionNo: envVersion === 'release' ? version : envVersion,
-    });
-  },
+  // 精简后保留必要方法（如需扩展在此添加）
 });
--- miniprogram-3/services/usercenter/fetchUsercenter.js
+++ miniprogram-3/services/usercenter/fetchUsercenter.js
@@ -1,18 +1,45 @@
 import { config } from '../../config/index';
 
-/** 获取个人中心信息 */
-function mockFetchUserCenter() {
-  const { delay } = require('../_utils/delay');
-  const { genUsercenter } = require('../../model/usercenter');
-  return delay(200).then(() => genUsercenter());
+const defaultUser = {
+  avatarUrl: '',
+  nickName: '',
+  phoneNumber: '',
+};
+
+const emptyCounts = [
+  { num: 0, name: '优惠券', type: 'coupon' },
+  { num: 0, name: '积分', type: 'point' },
+];
+
+const emptyOrderTags = [
+  { orderNum: 0, tabType: 5 },
+  { orderNum: 0, tabType: 10 },
+  { orderNum: 0, tabType: 40 },
+  { orderNum: 0, tabType: 0 },
+];
+
+const customerServiceInfo = {
+  servicePhone: '4006336868',
+  serviceTimeDuration: '每日 9:00-18:00',
+};
+
+function buildUserCenter(userInfo = defaultUser) {
+  return {
+    userInfo,
+    countsData: emptyCounts,
+    orderTagInfos: emptyOrderTags,
+    customerServiceInfo,
+  };
 }
 
-/** 获取个人中心信息 */
+/** 获取个人中心信息（仅返回存量数据，不再自动填充模拟） */
 export function fetchUserCenter() {
-  if (config.useMock) {
-    return mockFetchUserCenter();
+  // 若已有登录信息直接返回
+  const cachedUser = wx.getStorageSync('userInfo');
+  if (cachedUser) {
+    return Promise.resolve(buildUserCenter(cachedUser));
   }
-  return new Promise((resolve) => {
-    resolve('real api');
-  });
+
+  // 无缓存且无真实接口，直接返回空数据，不再自动登录或填充默认
+  return Promise.resolve(buildUserCenter(defaultUser));
 }
