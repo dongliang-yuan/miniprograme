a3d9ff70ed22adedf4c963657515f7f87c1a8b8e
dongliang.yuan <dongliang.yuan@decagames.cn>
2025-12-09 12:48:49 +0800

    Revert "update the database"
    This reverts commit 459fa54b2932f6d3baba0ee5fabb94cecb9bb03b.

--- miniprogram-3/services/usercenter/fetchUsercenter.js
+++ miniprogram-3/services/usercenter/fetchUsercenter.js
@@ -1,45 +1,18 @@
 import { config } from '../../config/index';
 
-const defaultUser = {
-  avatarUrl: '',
-  nickName: '',
-  phoneNumber: '',
-};
-
-const emptyCounts = [
-  { num: 0, name: '优惠券', type: 'coupon' },
-  { num: 0, name: '积分', type: 'point' },
-];
-
-const emptyOrderTags = [
-  { orderNum: 0, tabType: 5 },
-  { orderNum: 0, tabType: 10 },
-  { orderNum: 0, tabType: 40 },
-  { orderNum: 0, tabType: 0 },
-];
-
-const customerServiceInfo = {
-  servicePhone: '4006336868',
-  serviceTimeDuration: '每日 9:00-18:00',
-};
-
-function buildUserCenter(userInfo = defaultUser) {
-  return {
-    userInfo,
-    countsData: emptyCounts,
-    orderTagInfos: emptyOrderTags,
-    customerServiceInfo,
-  };
+/** 获取个人中心信息 */
+function mockFetchUserCenter() {
+  const { delay } = require('../_utils/delay');
+  const { genUsercenter } = require('../../model/usercenter');
+  return delay(200).then(() => genUsercenter());
 }
 
-/** 获取个人中心信息（仅返回存量数据，不再自动填充模拟） */
+/** 获取个人中心信息 */
 export function fetchUserCenter() {
-  // 若已有登录信息直接返回
-  const cachedUser = wx.getStorageSync('userInfo');
-  if (cachedUser) {
-    return Promise.resolve(buildUserCenter(cachedUser));
+  if (config.useMock) {
+    return mockFetchUserCenter();
   }
-
-  // 无缓存且无真实接口，直接返回空数据，不再自动登录或填充默认
-  return Promise.resolve(buildUserCenter(defaultUser));
+  return new Promise((resolve) => {
+    resolve('real api');
+  });
 }
